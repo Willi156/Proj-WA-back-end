049963ab74f6b30f51f9cf83d3c414159cb6bb06
Willi156 <williarduino@outlook.it>
2026-01-14 04:38:13 -0800

    fatte alcune modifiche da testare

--- src/main/java/com/critiverse/dao/JdbcUtenteDao.java
+++ src/main/java/com/critiverse/dao/JdbcUtenteDao.java
@@ -1,17 +1,18 @@
 package com.critiverse.dao;
 
-import com.critiverse.model.Utente;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.critiverse.model.Utente;
 
 @Repository
 public class JdbcUtenteDao implements UtenteDao {
@@ -28,8 +29,8 @@ public class JdbcUtenteDao implements UtenteDao {
         public Utente mapRow(ResultSet rs, int rowNum) throws SQLException {
             Utente u = new Utente();
             u.setId(rs.getLong("id"));
-            try { u.setNome(rs.getString("nome")); } catch (SQLException ignored) {}
-            try { u.setEmail(rs.getString("email")); } catch (SQLException ignored) {}
+            u.setNome(rs.getString("nome"));
+            u.setEmail(rs.getString("email"));
             return u;
         }
     }
